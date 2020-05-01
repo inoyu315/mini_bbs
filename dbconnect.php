a6bd06cb9c745582aaf44877f7e9672104cd29fb
github inoyu315 <yuto.41oniku@gmail.com>
2020-05-01 15:19:17 +0900

    add, commitコマンド追記
    GitHubへpushする練習

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $db = new PDO('mysql:dbname=mini_bbs;localhost=127.0.0.1;charset=utf8', 'root', 'root');
+} catch(PDOExeption $e) {
+    print 'DB接続エラー：' . $e->getMessage();
+}
+?>
